62beab35e1d5046f7ab5763eafadb7fa2dfa557f
AlanAruFlores <alanaruquipa242@gmail.com>
2023-04-27 18:30:34 -0300

    acerca de mi hecho

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//@ts-ignore;
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-acerca-de',
@@ -10,6 +12,34 @@ export class AcercaDeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const target = document.querySelector('.title-content')
+    const writer = new Typewriter(target, {
+      loop: false,
+      typeColor: '#EDEDED',
+      cursorColor: '#ff764d'
+
+    })
+    writer
+      .strings(
+        400,
+        "ACERCA DE MI",
+      )
+      .start()
+    //Description content animacion  
+      const descriptionElement = document.querySelector('.description-content')
+      const animacion = new Typewriter(descriptionElement, {
+        loop: true,
+        typeColor: '#ff764d',
+        cursorColor:'#EDEDED'
+      })
+      animacion
+        .strings(
+          600,
+          "Desarrollador FullStack",
+          "Estudiante de la UNLAM"
+        )
+        .start()
+      
   }
 
 }
