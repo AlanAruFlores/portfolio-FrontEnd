5ea640ca6796dc3208d4c888dff59117e2aabde9
AlanAruFlores <alanaruquipa242@gmail.com>
2023-04-26 21:35:28 -0300

    header terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    RedesSocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/redes-sociales/redes-sociales.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RedesSocialesComponent } from './redes-sociales.component';
+
+describe('RedesSocialesComponent', () => {
+  let component: RedesSocialesComponent;
+  let fixture: ComponentFixture<RedesSocialesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RedesSocialesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RedesSocialesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
